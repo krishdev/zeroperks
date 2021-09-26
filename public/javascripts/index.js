4beafe54a3b0c1fe845b540ea3aaa3e1169a21bf
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2021-09-26 12:02:40 -0500

    re-designing landing page, with my portfolio

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,8 @@
+import fontawesome from '@fortawesome/fontawesome-free';
+import fas from '@fortawesome/fontawesome-free-solid';
+
+// Add all icons to the library so you can use it in your page
+fontawesome.library.add(fas);
 import '../stylesheets/entries/all-entries.scss';
 import './navbar';
 import './login';
