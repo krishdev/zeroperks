f3cb457f73f51c78f36094250f27b77fb8c8fc5d
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2025-01-03 09:28:03 -0600

    Fix: error page

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(session({
 }));
 
 app.set('layout', 'main-layout');
-app.set("layout extractScripts", true);
+// app.set("layout extractScripts", true);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -65,6 +65,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  defaultLocals(req, res);
 
   // render the error page
   res.status(err.status || 500);
