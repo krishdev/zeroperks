aa28be9df8f05b834a0985972c0e9845bea60049
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2025-01-23 10:42:28 -0600

    fix test

--- routes/index.js
+++ routes/index.js
@@ -88,7 +88,7 @@ router.get('/topics', async function(req, res, next) {
   try {
     allBlogs = await getBlogs(3, '_featured=true');
     allCategories = await getAllCategories();
-    recentArticles = await getBlogs(6, '_sort=createdAt:DESC&_status=published');
+    recentArticles = await getBlogs(6, '_status=published&_sort=createdAt:DESC');
   } catch (error) {
     console.log(error);
   }
